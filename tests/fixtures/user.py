d6e0fadbc3fa54fe791d92118480615fc966232d
koladev <onaelmangabo@gmail.com>
2023-01-31 20:24:42 +0100

    :recycle: minor refactoring

--- tests/fixtures/user.py
+++ tests/fixtures/user.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 @pytest.fixture
 def user():
     user = User.objects.create_user(**{
-        "username": "Naruto",
-        "email": "naruto@konoha.com",
+        "username": "narutos",
+        "email": "naruto@datebayo.konoha",
         "password": "12345"
     })
 
