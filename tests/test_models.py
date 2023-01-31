cacb6eff02a58ee83ba2c165621159522e7e12e7
koladev <onaelmangabo@gmail.com>
2023-01-31 20:14:33 +0100

    :sparkles: add test project

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+import json
+
+import pytest
+
+
+class TestApiKeyModel:
+
+    def test_api_keys_created(self):
+        pass
+
+    def test_get_key(self):
+        pass
+
+    def test_create_key(self):
+        pass
+
+    def test_revoke_keys(self):
+        pass
