5119f131e29a0f354b2c019f774d91d2457be7e0
Ederene20 <atinhoruben@gmail.com>
2023-02-02 04:02:08 +0100

    feat: retrieve Api Key from the request header

--- rest_framework_simple_api_key/parser.py
+++ rest_framework_simple_api_key/parser.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest
+from rest_framework.exceptions import NotAuthenticated, AuthenticationFailed
+
 from rest_framework_simple_api_key.settings import package_settings
 
 
@@ -5,11 +8,26 @@ class APIKeyParser:
     keyword = package_settings.AUTHENTICATION_KEYWORD_HEADER
     message = "No API KEY provided."
 
-    def get(self):
-        pass
+    def get(self, request: HttpRequest):
+        api_key_header = getattr(package_settings, "API_KEY_HEADER", None)
 
-    def get_from_authorization(self):
-        pass
+        if api_key_header is not None:
+            return self.get_from_header(request, api_key_header)
 
-    def get_from_header(self):
-        pass
+        return self.get_from_authorization(request)
+
+    def get_from_authorization(self, request: HttpRequest):
+        authorization = request.META.get("HTTP_AUTHORIZATION")
+
+        if not authorization:
+            raise NotAuthenticated
+
+        try:
+            _, key = authorization.split("{} ".format(self.keyword))
+        except ValueError:
+            raise AuthenticationFailed
+
+        return key
+
+    def get_from_header(self, request: HttpRequest, name: str):
+        return request.META.get(name) or None
