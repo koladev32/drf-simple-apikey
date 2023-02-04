f227fdf6ac0cf31cc8039ffee54f91723788f8be
koladev <onaelmangabo@gmail.com>
2023-02-04 18:57:46 +0100

    :pencil2: fix typos

--- rest_framework_simple_api_key/parser.py
+++ rest_framework_simple_api_key/parser.py
@@ -6,7 +6,10 @@ from rest_framework.exceptions import NotAuthenticated, AuthenticationFailed
 
 
 class APIKeyParser:
-    """ """
+    """
+    This is a custom parser used to retrieve the API Key from the
+    authorization header. You can add custom parsing validation here.
+    """
 
     keyword = settings.SIMPLE_API_KEY.get("AUTHENTICATION_KEYWORD_HEADER")
     message = "No API key provided."
