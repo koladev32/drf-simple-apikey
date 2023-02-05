db69565217d57abcae4770c4c3462af60155d8f6
Ederene20 <atinhoruben@gmail.com>
2023-02-05 21:44:33 +0100

    test: test the custom command generate_fernet_key

---
+++ tests/test_custom_django_command.py
@@ -0,0 +1,18 @@
+import pytest
+
+from io import StringIO
+
+from django.core.management import call_command
+
+
+def test_command_generate_fernet_key():
+    out = StringIO()
+    args = []
+    opts = {}
+
+    call_command('generate_fernet_key', *args, **opts, stdout=out)
+
+    message = out.getvalue()
+
+    assert message is not None
+    assert type(message) is str
