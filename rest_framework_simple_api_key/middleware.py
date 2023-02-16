d70c21363cde4116c56946c2e309637d28b2ba30
Ederene20 <atinhoruben@gmail.com>
2023-02-16 15:53:14 +0100

    fix: fix middleware issue

--- rest_framework_simple_api_key/middleware.py
+++ rest_framework_simple_api_key/middleware.py
@@ -1,16 +1,14 @@
 from django.conf import settings
-from django.contrib.auth.middleware import get_user
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.functional import SimpleLazyObject
+
+from rest_framework_simple_api_key.backends import APIKeyAuthentication
 
 
 class ApiKeyAuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        assert hasattr(request, 'session'), (
-            "The Django Rest Framework Simple API Key authentication middleware requires session middleware "
-            "to be installed. Edit your MIDDLEWARE%s setting to insert "
-            "'django.contrib.sessions.middleware.SessionMiddleware' before "
-            "'rest_framework_simple_api_key.middleware.ApiKeyAuthenticationMiddleware'."
-        ) % ("_CLASSES" if settings.MIDDLEWARE is None else "")
+        api_key_authentication = APIKeyAuthentication()
+        entity, _ = api_key_authentication.authenticate(request)
         custom_entity_name = settings.SIMPLE_API_KEY["custom_entity_name"]
-        setattr(request, custom_entity_name, SimpleLazyObject(lambda: get_user(request)))
+
+        if not hasattr(request, custom_entity_name):
+            setattr(request, custom_entity_name, entity)
