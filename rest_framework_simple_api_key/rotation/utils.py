bb7532ff3283e63f92c3416835af755a2613a8c4
koladev <onaelmangabo@gmail.com>
2023-09-16 18:41:32 +0100

    :recycle: minor refactoring

--- rest_framework_simple_api_key/rotation/utils.py
+++ rest_framework_simple_api_key/rotation/utils.py
@@ -1,5 +1,6 @@
 from django.core.cache import cache
 from django.apps import apps
+from django.utils import timezone
 
 from rest_framework_simple_api_key.settings import package_settings
 
@@ -7,16 +8,28 @@ from rest_framework_simple_api_key.settings import package_settings
 def get_rotation_status():
     rotation_status = cache.get("rotation_status")
 
-    if not rotation_status:
+    if rotation_status is None:  # We should check for 'None' specifically because the cached value could be False
         # Lazy load the Rotation model
         Rotation = apps.get_model('rest_framework_simple_api_key_rotation', 'Rotation')
 
-        config = Rotation.objects.last()
-        rotation_status = config.is_rotation_enabled if config else False
+        # Filter the latest rotation that is enabled
+        config = Rotation.objects.filter(is_rotation_enabled=True).order_by('-started').first()
+
+        # If we have a rotation config and its 'ended' date has passed, update it
+        if config and config.ended and config.ended <= timezone.now():
+            config.is_rotation_enabled = False
+            config.save()
+            rotation_status = False
+        elif config:
+            rotation_status = True
+        else:
+            rotation_status = False
+
+        # Cache the rotation status
         cache.set(
             "rotation_status",
             rotation_status,
-            package_settings.ROTATION_PERIOD.total_seconds(),
-        )  # Cache for the rotation period
+            package_settings.ROTATION_PERIOD.total_seconds() if rotation_status else None,
+        )  # Cache for the rotation period if true
 
     return rotation_status
