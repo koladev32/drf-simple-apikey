7aad2a7662ed42a44ebf8daf5507fdfc556d3be0
koladev <onaelmangabo@gmail.com>
2024-05-23 02:19:58 +0100

    :memo: add warnings message

--- rest_framework_simple_api_key/__init__.py
+++ rest_framework_simple_api_key/__init__.py
@@ -4,6 +4,16 @@ Django REST framework Simple API Key Configurations
 
 import django
 
+import warnings
+
+warnings.warn(
+    "This package, 'djangorestframework-simple-apikey', has been renamed to 'drf-apikey' and will no longer be updated."
+    "Please switch to the new package to continue receiving updates and support. "
+    "For more information and migration instructions, please visit: https://djangorestframework-simple-apikey.readthedocs.io/en/latest/migrating.html",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 
 __title__ = "Django REST framework Simple API Key"
 __author__ = ["Kolawole Mangabo", "Ruben Atinho"]
